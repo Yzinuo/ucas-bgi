13bb705a9f7329c4978d9fe9f26a31ad3f80b4bd
Yang zinuo <13454453+yang-zinuo@user.noreply.gitee.com>
2026-08-13 18:10:17 +0800

    Feat:完成github action部署

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  // GitHub Pages serves project sites from /<repository>/.
-  base: process.env.BASE_PATH || '/',
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '.', '')
+
+  return {
+    // GitHub Pages serves project sites from /<repository>/.
+    base: env.BASE_PATH || '/',
+    plugins: [react()],
+  }
 })
